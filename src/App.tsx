06ec9e1a354f247bb97e0f6606454c250cbc2dda
Prachi Patel <prachipatel5441@gmail.com>
2024-09-29 22:28:55 +0530

    remove deployment errors

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <h1 className='text-center text-yellow-300 text-5xl mt-8 font-bold'>Lets try, Extract Text from your image!       
     </h1>
     <span className="flex items-center justify-center mt-4">
-        {/* Example GIF */}
+    
         <img src="/face-kawaii.gif" alt="Demo GIF" className="w-20 h-20" />
       </span>
     <span className='flex flex-col-reverse md:flex-row-reverse   items-baseline'>
--- src/component/dragzone.tsx
+++ src/component/dragzone.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import { Group, Text, Card,rem } from '@mantine/core';
 import { CopyButton, Button } from '@mantine/core';
